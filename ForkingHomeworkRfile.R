5abf51e56deaf38547cdf770cfac0b427cf3a6ff
ryangreen3 <126120459+ryangreen3@users.noreply.github.com>
2023-04-03 17:27:44 -0400

    Update ForkingHomeworkRfile.R

--- ForkingHomeworkRfile.R
+++ ForkingHomeworkRfile.R
@@ -10,3 +10,5 @@ diamonds%>%
   filter(price > 7000 & price < 10000)%>%
   arrange(desc(carat))%>%
   slice(1:20)
+
+diamonds[cut == "Premium"][price > 7000 & price < 10000][c(1:20), c("carat", "cut", "price")][order(-carat)]
